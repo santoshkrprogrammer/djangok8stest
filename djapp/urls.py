fb7d879db11c6a850041f8bccd8ddc1f64130dfd
santoshkrprogrammer <santoshkrm567@gmail.com>
2024-12-29 20:04:05 +0545

    added a change in ymlfile

--- djapp/urls.py
+++ djapp/urls.py
@@ -5,4 +5,6 @@ from .views import *
 
 urlpatterns = [
     path('', home),
+     path('path1/', newpath),
+
 ]
--- djapp/views.py
+++ djapp/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("This is a test django project for checkby santosh")
+
+def newpath(request):
+    return HttpResponse("This is a new path")
